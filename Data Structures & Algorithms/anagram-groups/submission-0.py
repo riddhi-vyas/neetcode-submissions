82f55d6ac052456b6e616a3b7ec0d44bfe69000e
Riddhi Vyas <162080414+riddhi-vyas@users.noreply.github.com>
2026-04-18 18:14:30 -0700

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        #edge case
+        if not strs:
+            return [[]]
+        str_map = {} #dict to store string_sorted as key, anagram list as values
+        for word in strs:
+            word_sorted = str(sorted(word))
+            if word_sorted not in str_map:
+                str_map[word_sorted] = []
+            str_map[word_sorted].append(word)
+        return list(str_map.values())
