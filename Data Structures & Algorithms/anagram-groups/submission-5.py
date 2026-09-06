ad9982edcc4116b4fdd170345687e5aa03b309be
Riddhi Vyas <162080414+riddhi-vyas@users.noreply.github.com>
2026-09-06 15:39:20 -0700

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.py
@@ -0,0 +1,13 @@
+# Time comp: O(n k log k), n is total strings in strs, klogk is sorting comp for each string
+#Space comp: O(n k)
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        if not strs:
+            return []
+        str_map = {}
+        for item in strs:
+            sorted_item = str(sorted(item))
+            if sorted_item not in str_map:
+                str_map[sorted_item] = []
+            str_map[sorted_item].append(item)
+        return list(str_map.values())
