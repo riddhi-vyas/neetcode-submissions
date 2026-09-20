d6e191f007ed31abff7965432f9ef0747af6e6b6
Riddhi Vyas <162080414+riddhi-vyas@users.noreply.github.com>
2026-09-20 16:33:28 -0700

    Add: invert-a-binary-tree - submission-1

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-1.py
@@ -0,0 +1,18 @@
+# Recursive DFS Approach
+# Time comp: O(n)—visit every node once.
+# Space comp: O(h)—recursion stack; O(log n) for a balanced tree, O(n) for a skewed tree.
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
